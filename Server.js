57d4c9c6808d69abd5ed008eaf0ee1c4e44fc892
Samson Timoner <samson@ai.mit.edu>
2015-06-17 17:32:46 -0400

    broke file up into pieces

---
+++ Server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+var httpServer; 
+// var io = require('socket.io')(httpServer);
+
+// =============
+// Configuration
+// =============
+var port                = 3002;
+var requireSecureServer = false;
+
+
+// =============
+// Setup Server
+// =============
+
+httpServer = require('http').Server(app);
+
+// Some debugging
+httpServer.listen(port, function () {
+  console.log('Server','ServerId',"Listening on * at port " + port);
+});
+
+// Directory
+app.use(express.static(__dirname));
+
+app.get('/*/*', function(req, res){
+	// var match = req.url.match('/(\S+)/(\S+)[/?$]/');
+	//var match = req.url.match(/\/(\S+)\/(\S+)(\/|$)+/);
+	// var match = req.url.match(/\/(\S+)\/(\S+)/);
+	// console.log(match[1]," ",match[2]);
+	console.log("goodbuy: " + req.url);
+	// var urlParse = url.parse(req.url);
+	// ignore the company name and room name.
+	companyName = 'a';
+	roomName = 'b';
+	res.sendFile('index.html');
+
+	/* 
+	if ( (isUser && authorizedUser(userId,companyName,roomname)) ||
+	     (isCamera && authorizedCamera(cameraId,compnayName,roomname)) ) {
+   		res.sendFile(path.join(__dirname,'/../Public/index.html'));
+   	} else {
+   	    res.send("Not Authorized");
+   	}
+   	*/
+});
+ 
+// Not being used at all.
+app.get('/', function(req, res){
+  console.log("hello" + req.url);
+  res.sendFile(__dirname + '/../Public/index.html');
+});
+
+
